6ecbb6cb0c47a0b31b9c6e3a4b6dfa885d3bb92a
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-04-05 10:28:00 +0200

    Use the NgOnInit Lifecycle Hook

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -3,18 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+    <div>
+      <div>
+        <!-- {{donut | json}} -->
+        {{donut.name}}
+        {{donut.price | currency}}
+      </div>
+    </div>
   `,
   styles: [
   ]
 })
 export class DonutListComponent implements OnInit {
+  donut!: any;
+  donuts!: any[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 199,
+        description: 'For the pure chocoholic.'
+      },
+      {
+        id: '3u98kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Sticky perfection.'
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel.'
+      }
+    ];
+
+    this.donut = this.donuts[0];
   }
 
 }
