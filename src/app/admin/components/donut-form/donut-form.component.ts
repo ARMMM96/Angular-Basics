d1061d278a685762c1116e1f43e805036dc02521
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-04-09 15:25:05 +0200

    select and option Rendering and Binding

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -4,15 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'donut-form',
   template: `
     <form class="donut-form" #form="ngForm">
-      <label>
-        <span>Name</span>
-        <input 
-            type="text" 
-            name="name"
-            class="input"
-            ngModel
-            />
+        <label>
+          <span>Name</span>
+          <input 
+              type="text" 
+              name="name"
+              class="input"
+              ngModel
+              />
         </label>
+
+        <label>
+          <span>Icon</span>
+
+          <select
+            name="icon"
+            class="input input--select"
+            ngModel
+          >
+          <option *ngFor="let icon of icons" [ngValue]="icon">{{icon}}</option>
+
+          </select>
+        </label>
+
+
         <label>
         <span>Price</span>
         <input type="number" name="price" class="input" ngModel />
@@ -21,15 +36,15 @@ import { Component, OnInit } from '@angular/core';
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" [value]="undefined" ngModel>
+          <input type="radio" name="promo" [value]="undefined" ngModel/>
           <span>None</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="new" ngModel>
+          <input type="radio" name="promo" value="new" ngModel/>
           <span>New</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="limited" ngModel>
+          <input type="radio" name="promo" value="limited" ngModel/>
           <span>Limited</span>
         </label>
       </div>
@@ -61,9 +76,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutFormComponent implements OnInit {
 
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'just-chocolate',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon',
+  ];
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
