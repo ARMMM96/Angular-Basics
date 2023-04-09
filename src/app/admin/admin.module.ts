af53f88a8e99b07ccf9dee02b2f5b3ddb27cc213
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-04-09 11:54:41 +0200

    Use NgForm with a Template Ref Variable

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // containers
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
@@ -18,7 +19,8 @@ import { DonutFormComponent } from './components/donut-form/donut-form.component
     DonutFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, 
+    FormsModule
   ],
   exports: [
     DonutListComponent,
